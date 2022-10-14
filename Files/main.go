a919fdba3e08797a8cfd644ec47ca447d798919c
Joshua Folivi <joshua.folivi@gmail.com>
2022-10-14 17:19:05 +0100

    Finalised read from and write into files implementation

--- Files/main.go
+++ Files/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	data := []byte("Hello World!")
+
+	file1, _ := os.Create("data2")
+	defer file1.Close()
+
+	bytes, _ := file1.Write(data)
+	fmt.Printf("Wrote %d bytes to file.\n", bytes)
+
+	file2, _ := os.Open("data2")
+	defer file2.Close()
+
+	read := make([]byte, len(data))
+	bytes, _ = file2.Read(read)
+	fmt.Printf("Read %d bytes from file.\n", bytes)
+	fmt.Println(string(read))
 }
