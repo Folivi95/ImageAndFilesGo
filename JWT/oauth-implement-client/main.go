1b2ac2abbb69951b88ad85f7525ea6b43062a24a
Joshua Folivi <joshua.folivi@gmail.com>
2022-10-16 13:46:03 +0100

    finalized implementation for JWT authentication

--- JWT/oauth-implement-client/main.go
+++ JWT/oauth-implement-client/main.go
@@ -3,20 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/golang-jwt/jwt/v4"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 )
 
+var mySigningKey = []byte("dummysignedkey")
+
 func main() {
-	fmt.Println("Welcome to JWT Application")
-	jwtToken, err := GenerateJWTToken()
-	if err != nil {
-		log.Fatal("Failed to generate token: ", err)
-	}
-
-	fmt.Printf("JWT Token: %v", jwtToken)
-
 	handleRequests()
 }
 
@@ -29,7 +24,7 @@ func GenerateJWTToken() (string, error) {
 	claims["user"] = "@binator_1308"
 	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
 
-	tokenString, err := token.SigningString()
+	tokenString, err := token.SignedString(mySigningKey)
 
 	if err != nil {
 		fmt.Errorf("generating JWT Token failed")
@@ -41,11 +36,23 @@ func GenerateJWTToken() (string, error) {
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	validToken, err := GenerateJWTToken()
+
+	// make http request
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:9000", nil)
+	req.Header.Set("Token", validToken)
+
+	res, err := client.Do(req)
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
 
-	fmt.Fprintf(w, validToken)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	fmt.Fprintf(w, string(body))
 }
 
 func handleRequests() {
--- JWT/oauth-implement-server/main.go
+++ JWT/oauth-implement-server/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/golang-jwt/jwt/v4"
 	"log"
 	"net/http"
 )
+
+var mySigningKey = []byte("dummysignedkey")
 
 func main() {
 	handleRequests()
@@ -14,6 +17,30 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.Handle("/", isAuthorized(homePage))
 	log.Fatal(http.ListenAndServe(":9000", nil))
+}
+
+func isAuthorized(endpoint func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("there was an error")
+				}
+
+				return mySigningKey, nil
+			})
+
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+			fmt.Fprintf(w, "Not authorized")
+		}
+	})
 }
